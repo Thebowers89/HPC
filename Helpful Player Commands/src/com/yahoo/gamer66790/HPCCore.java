156eb08e011d691119f85a9546a7d8ba30d371dc
jakeyray18 <jakeyray18@gmail.com>
2015-04-28 00:07:06 -0500

    Organized registering command executors

--- Helpful Player Commands/src/com/yahoo/gamer66790/HPCCore.java
+++ Helpful Player Commands/src/com/yahoo/gamer66790/HPCCore.java
@@ -1,29 +1,28 @@
 package com.yahoo.gamer66790;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HPCCore extends JavaPlugin{
-	
+
 	@Override
 	public void onEnable() {
-		getCommand("heal").setExecutor(new HealCommand(this));
-		getCommand("feed").setExecutor(new FeedCommand(this));
-		getCommand("poc").setExecutor(new PocCommand(this));
-		getCommand("tts").setExecutor(new TTSCommand(this));
-		getCommand("skull").setExecutor(new SkullCommand());
-		getCommand("killit").setExecutor(new KillitCommand());
-		getCommand("tillik").setExecutor(new TillikCommand());
-		getCommand("playnice").setExecutor(new PlayniceCommand(this));
-		getCommand("playmean").setExecutor(new PlaymeanCommand());
-		getCommand("hpcreload").setExecutor(new ReloadCommand(this));
-		
-		this.saveDefaultConfig();
+
+		saveDefaultConfig();
+
+        registerCommands();
 	}
-	
-	@Override
-	public void onDisable() {
-		
-	}
-	
+
+	private void registerCommands() {
+        getCommand("heal").setExecutor(new HealCommand(this));
+        getCommand("feed").setExecutor(new FeedCommand(this));
+        getCommand("poc").setExecutor(new PocCommand(this));
+        getCommand("tts").setExecutor(new TTSCommand(this));
+        getCommand("skull").setExecutor(new SkullCommand());
+        getCommand("killit").setExecutor(new KillitCommand());
+        getCommand("tillik").setExecutor(new TillikCommand());
+        getCommand("playnice").setExecutor(new PlayniceCommand(this));
+        getCommand("playmean").setExecutor(new PlaymeanCommand());
+        getCommand("hpcreload").setExecutor(new ReloadCommand(this));
+    }
+
 }
